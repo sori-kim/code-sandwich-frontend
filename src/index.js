dde4f2ca1824e2cd5245d0e3899c055a3ab2c63c
rimuuu <soryonew@gmail.com>
2020-05-25 16:54:53 +0900

    [Add] 초기세팅 완료 - 페이지별 폴더 구성완료 - sass, reat-router 설치 완료 - common.scss 작성

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
+import "./styles/reset.scss";
+import "./styles/_common.scss";
+
+ReactDOM.render(<Routes />, document.getElementById("root"));
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+    return <div>나는 메인입니다</div>;
+  }
+}
+
+export default Main;
