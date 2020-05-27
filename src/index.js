30322f4f790fdec87fb4ff613253769b274cf960
mokyoungg <mokyoungg@gmail.com>
2020-05-27 16:13:23 +0900

    [Fix] 충돌해결 - index.js 충돌부분 해결

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import "./styles/reset.scss";
 import "./styles/_common.scss";
-import Menu from "./pages/Menu/Menu";
 
-ReactDOM.render(<Menu />, document.getElementById("root"));
+ReactDOM.render(<Routes />, document.getElementById("root"));
