7f6ab674cddc45704de1cda0bda299f90bd1f5cf
rimuuu <soryonew@gmail.com>
2020-05-28 10:17:26 +0900

    [Add] main 기능 추가 - menu section filter 기능 추가 - 이미지 hover animation 추가

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,23 +1,35 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import MenuSection from "../Main/MenuSection/MenuSection";
-import Event from "./Event/Event"
+import Event from "./Event/Event";
 import Footer from "../../components/Footer/Footer";
 import "./Main.scss";
 import AutoPlay from "./AutoPlay/AutoPlay";
+import map from "../../images/icon_map.png";
+import franchise from "../../images/icon_franchise.png";
 
 class Main extends React.Component {
   render() {
     return (
-        <>
-         <Header />
-         <div className="Main">
+      <>
+        <Header />
+        <div className="Main">
+          <div className="main_button">
+            <div className="button_order">
+              <img src={franchise} alt="icon" />
+              <p>주문하기</p>
+            </div>
+            <div className="button_map">
+              <p>매장찾기</p>
+              <img src={map} alt="icon" />
+            </div>
+          </div>
           <AutoPlay />
           <MenuSection />
           <Event />
-         </div>
-         <Footer />
-        </>
+        </div>
+        <Footer />
+      </>
     );
   }
 }
--- src/pages/Main/MenuSection/MenuItem/MenuItem.js
+++ src/pages/Main/MenuSection/MenuItem/MenuItem.js
@@ -1,17 +1,14 @@
 import React from "react";
-import "./MenuItem.scss"
+import "./MenuItem.scss";
 
-
-class MenuItem extends React.Component{
-    render(id,image,name,summary){
-        return(
-            <div className="menu" key={this.props.id}>
-                <img src={this.props.image} alt="menu" />
-                <div className="menu_name">{this.props.name}</div>  
-                <div className="menu_desc">{this.props.summary}</div>
-            </div>
-        )
-    }
+export default class MenuItem extends React.Component {
+  render(id, image, name, summary) {
+    return (
+      <div className="menu" key={this.props.id}>
+        <img src={this.props.image} alt="menu" />
+        <div className="menu_name">{this.props.name}</div>
+        <div className="menu_desc">{this.props.summary}</div>
+      </div>
+    );
+  }
 }
-
-export default MenuItem; 
