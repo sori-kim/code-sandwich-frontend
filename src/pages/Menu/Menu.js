6d7b86605618f8c5c649148d4846116316b028c6
JIA <jiakim1222@gmail.com>
2020-06-01 11:13:43 +0900

    다른 분 파일을 수정했던 내역이 있어 conflict 방지용으로 복붙

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -3,36 +3,91 @@ import MenuList from "./MenuList/MenuList";
 import TopImage from "./TopImage/TopImage";
 import Middle from "./Middle/Middle";
 import Header from "../../components/Header/Header";
+import Nav from "../../components/Nav/Nav";
 import Footer from "../../components/Footer/Footer";
 import "./Menu.scss";
-
+​
 class Menu extends Component {
-  state = {
-    sandwich: [],
-  };
-
-  componentDidMount() {
-    fetch("http://localhost:3000/data/data.json")
-      .then((response) => response.json())
-      .then((response) => this.setState({ sandwich: response.data }));
-  }
-
-  render() {
-    const { sandwich } = this.state;
-    return (
-      <div>
-        <Header />
-        <div className="menuWrap">
-          <TopImage />
-          <Middle />
-          <div className="menuLine">
-            <MenuList sandwich={sandwich} />
+    state = {
+        sandwich: [],
+        new_sandwich: [],
+        cl_sandwich: [],
+        fl_sandwich: [],
+        pm_sandwich: [],
+        bf_sandwich: [],
+        id: ""
+    }
+​
+    componentDidMount() {
+      fetch("http://10.58.3.228:8000/product/sandwich")
+        .then((res) => res.json())
+        .then((res) => this.setState({ sandwich: res.sandwiches}))
+        .then((res) =>
+          this.setState({
+            new_sandwich: this.state.sandwich,
+            cl_sandwich: this.state.sandwich.filter((res) =>
+              res.subcategory_id === 1
+            ),
+            fl_sandwich: this.state.sandwich.filter((res) =>
+              res.subcategory_id === 2
+            ),
+            pm_sandwich: this.state.sandwich.filter((res) =>
+              res.subcategory_id ===3
+            ),
+            bf_sandwich: this.state.sandwich.filter((res) =>
+              res.subcategory_id === 4
+            ),
+          })
+        )
+    }
+​
+    
+    handleOnClick = (e) => {
+      const category = e.target.innerText;
+      let arr = [];
+      if(category === "All"){
+        arr = this.state.sandwich;
+      } else if (category === "클래식") {
+        arr = this.state.cl_sandwich;
+      } else if (category === "프레쉬&라이트") {
+        arr = this.state.fl_sandwich;
+      } else if (category === "프리미엄") {
+        arr = this.state.pm_sandwich;
+      } else if (category === "아침메뉴"){
+        arr = this.state.bf_sandwich;
+      }
+      this.setState({ new_sandwich: arr });
+    };
+    
+​
+    render(){
+     const {
+      sandwich,
+      new_sandwich,
+      cl_sandwich,
+      fl_sandwich,
+      pm_sandwich,
+      bf_sandwich,
+    } = this.state;
+        console.log(this.state)
+        return(
+          <div>
+            <Header />
+            <div className="menuWrap">
+              <div className="menuTop">
+                <TopImage />
+              </div>
+              <div className="menuMiddle">
+                <Middle sandwich={sandwich} new_sandwich={new_sandwich} cl_sandwich={cl_sandwich} fl_sandwich={fl_sandwich} pm_sandwich={pm_sandwich} bf_sandwich={bf_sandwich} handleOnClick={this.handleOnClick} />
+              </div>
+              <div className='menuLine'>
+                <MenuList sandwich={new_sandwich} />
+              </div>
+            </div>
+            <Footer />
           </div>
-        </div>
-        <Footer />
-      </div>
     );
   }
 }
-
+​
 export default Menu;
--- src/pages/Menu/MenuBox/MenuBox.js
+++ src/pages/Menu/MenuBox/MenuBox.js
@@ -1,27 +1,45 @@
 import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
 import "./MenuBox.scss";
 import "../../../styles/reset.scss";
-
+​
 class MenuBox extends Component {
-  render() {
-    return (
-      <div className="menuBox">
-        <div className="menuImg">
-          <img alt="We are sorry" src={this.props.image} />
-        </div>
-        <p className="name">{this.props.name}</p>
-        <p className="ename">{this.props.ename}</p>
-        <p className={this.props.kcal > 0 ? "kcal" : "nokcal"}>
-          {this.props.kcal} kcal
-        </p>
-        {/*<div className='summary'>
-                <p> 육즙이쫙 <br />
-                풍부한 비프 스테이크의 풍미가 입안 한가득
-                </p>
-                </div>*/}
-      </div>
-    );
-  }
+    state=({
+      animation: false
+    })
+​
+    animationON =(e) => {
+      console.log('바뀐다')
+      this.setState({animation: true});
+    }
+​
+    animationOff =(e) => {
+      this.setState({animation: false});  
+    }
+​
+​
+    render(){
+        return(
+        //<Link to="/menu_details">
+          <div className="menuBoxWrap" onMouseEnter={this.animationON} onMouseLeave={this.animationOff}>
+            <div className='menuBox'>
+              <div className ='menuImg'>
+                <img alt='We are sorry' src={this.props.image} />
+              </div>
+              <p className='name'>{this.props.name}</p>
+              <p className='ename'>{this.props.ename}</p>
+              <p className={this.props.kcal > 0 ? "kcal":"nokcal"}>{this.props.kcal} kcal</p>
+            </div>
+            <div className={this.state.animation? "summaryBox":"noBox"}>
+              <p className='nameH'>{this.props.name}</p>
+              <p className='enameH'>{this.props.ename}</p>
+              <p className="summary">{this.props.summary}</p>
+              <img className="summaryImg" src="http://subway.co.kr/images/menu/icon_view.png" alt="자세히보기" />
+            </div>
+          </div>
+        //</Link>
+        )
+    }
 }
-
+​
 export default MenuBox;
