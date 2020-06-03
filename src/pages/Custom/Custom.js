43b85c245da5222b870d11d84ff89a625fbd1635
rimuuu <soryonew@gmail.com>
2020-06-04 02:18:48 +0900

    [Add] Custom 페이지 빵선택 기능구현 - 유저가 빵을 선택했을때 커스텀 페이지 정보에 새로운 빵으로 변경 - 토핑 선택 기능과 연결해서 백에 데이터 보낼 예정

--- src/pages/Custom/Custom.js
+++ src/pages/Custom/Custom.js
@@ -4,34 +4,78 @@ import Footer from "../../components/Footer/Footer";
 import OrderBox from "../../components/OrderBox/OrderBox";
 import Ingredient from "./Ingredient/Ingredient";
 import Bread from "../Custom/Bread/Bread";
+import { URL } from "../../Config";
 import "./Custom.scss";
 
 export default class Custom extends React.Component {
   state = {
     isActive: false,
     isShown: "noshow_bread",
+    customized: [],
     default_ingredients: [],
+    added_ingredients: [],
     image_url: [],
     bread: [],
     cart: [],
   };
 
   componentDidMount() {
+    let received_topping = localStorage.getItem("testObject");
+    if (received_topping !== null) {
+      this.setState({
+        added_ingredients: this.state.added_ingredients.concat(
+          received_topping
+        ),
+      });
+    }
     fetch(
-      `http://10.58.1.217:8000/product/sandwich/customization/?product_id=${this.props.match.params.key}`
+      `${URL}/product/sandwich/customization/?product_id=${this.props.match.params.key}`
     )
       .then((res) => res.json())
       .then((res) =>
         this.setState({
           default_ingredients: res.default_ingredients,
+          customized: res.default_ingredients,
+          bread: res.all_bread.filter(
+            (bread) => bread.name.includes("top") || bread.name.includes("플랫")
+          ),
         })
       );
   }
 
   //Looks Good 버튼 눌렀을때 빵 선택 완료되고 커스텀 페이지로 돌아오기
-  looksgood = () => {
+  /*소리 looksgood
+  looksgood = (newBread) => {
+    const { default_ingredients, added_ingredients } = this.state;
     this.setState({
       isShown: "noshow_bread",
+      added_ingredients: newBread,
+      customized: default_ingredients,
+    });
+    if (default_ingredients.some((el) => el.ingredient_category_id === 1)) {
+      let toRemove = newBread;
+      let indexTop = 0;
+      let indexBottom = default_ingredients.length - 1;
+
+      this.setState({ customized: default_ingredients });
+      let breadBot = default_ingredients.splice(
+        indexBottom,
+        1,
+        this.state.added_ingredients
+      );
+     this.setState({ customized: custom });
+    }
+  };*/
+  looksgood = (newBread) => {
+    const { default_ingredients, added_ingredients } = this.state;
+    this.setState({
+      isShown: "noshow_bread",
+      added_ingredients: newBread,
+    });
+    let arr = default_ingredients.slice(1, default_ingredients.length - 1);
+    arr = newBread.concat(arr);
+    this.setState({
+      customized: arr,
     });
   };
 
@@ -42,19 +86,6 @@ export default class Custom extends React.Component {
     });
   };
 
-  //빵 데이터 fetch
-  handleBreadFetch = () => {
-    fetch("http://10.58.1.217:8000/product/sandwich/customization/bread/")
-      .then((res) => res.json())
-      .then((res) =>
-        this.setState({
-          bread: res.all_bread.filter(
-            (bread) => bread.name.includes("top") || bread.name.includes("플랫")
-          ),
-        })
-      );
-  };
-
   render() {
     return (
       <>
@@ -63,7 +94,7 @@ export default class Custom extends React.Component {
           <div className="custom_wrapper">
             <Bread
               isShown={this.state.isShown === "show_bread"}
-              handleGood={this.looksgood}
+              looksGood={this.looksgood}
               bread={this.state.bread}
             />
             <div
@@ -74,8 +105,9 @@ export default class Custom extends React.Component {
               }
               onClick={this.handleBreadFetch}
             >
-              {this.state.default_ingredients.map((things) => (
+              {this.state.customized.map((things) => (
                 <Ingredient
+                  name={things.name}
                   image={things.image_url}
                   handleIngredients={this.handleIngredients}
                   price={things.price}
